1edc1ed72b70b2a7aea58c07bc18ea5a19196ffc
Pronob <pronobmozumder.info@gmail.com>
2021-01-28 04:10:40 +0600

    static conf. process going on

--- jobportal/mainapp/urls.py
+++ jobportal/mainapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('jobs/', views.jobs, name="jobs"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
 ]
--- jobportal/mainapp/views.py
+++ jobportal/mainapp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
 def index(request):
     return render(request, 'index.html')
+
+def jobs(request):
+    return render(request, 'browse-job.html')
